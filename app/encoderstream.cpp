5d74ae22af95cc6589faa7fe39afd7c4b4bbf1ad
philstopford <phil.stopford@gmail.com>
2025-04-06 14:32:25 -0700

    Update encoderstream.cpp

--- app/encoderstream.cpp
+++ app/encoderstream.cpp
@@ -20,6 +20,9 @@
 #include <cmath>
 #include <ctime>
 #include <algorithm>
+#if defined(Q_OS_WIN64)
+#include <windows.h>
+#endif
 
 #define rnd(num) static_cast<int>(round(num))
 
@@ -294,7 +297,53 @@ void EncoderStream::encode()   // Encode
         m_message = tr("An unknown error occurred!\n Possible FFMPEG not installed.\n");
         emit onEncodingInitError(m_message);
     }
+
+#if defined(Q_OS_WIN64)
+    set_process_prio_win();
+#endif
 }
+
+// Completely untested.
+#if defined(Q_OS_WIN64)
+void EncoderStream::set_process_prio_win()
+{
+    // Get the process handle
+    HANDLE hProcess = OpenProcess(PROCESS_SET_INFORMATION, FALSE, m_pProcessEncoding.processId());
+    if (hProcess == nullptr) {
+        qWarning("Failed to open process");
+        return;
+    }
+
+    auto nicelevel;
+    switch (_prio)
+    {
+        case Constants::lowest:
+            nicelevel = IDLE_PRIORITY_CLASS;
+            break;
+        case Constants::low:
+            nicelevel = BELOW_NORMAL_PRIORITY_CLASS;
+            break;
+        case Constants::normal:
+        default:
+            nicelevel = NORMAL_PRIORITY_CLASS;
+            break;
+        case Constants::high:
+            nicelevel = ABOVE_NORMAL_PRIORITY_CLASS;
+            break;
+        case Constants::highest:
+            nicelevel = HIGH_PRIORITY_CLASS;
+            break;
+    }
+
+    // Set the priority class
+    if (!SetPriorityClass(hProcess, priority)) {
+        qWarning("Failed to set process priority");
+    }
+
+    // Close the process handle
+    CloseHandle(hProcess);
+}
+#endif
 
 void EncoderStream::progress()   // Progress
 {
